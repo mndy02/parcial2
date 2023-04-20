28f71e3f6045675fd9b4ff06fe0bce46871e192c
mndy02 <126424519+mndy02@users.noreply.github.com>
2023-04-20 17:13:10 -0600

    parcial.py

--- parcial2/parcial.py
+++ parcial2/parcial.py
@@ -1,22 +1,23 @@
-'''Escribe un programa que, al recibir como dato un número entero positivo N, calcule el resultado de la siguiente serie:
-1 + (1/2) + (1/3) + (1/4) + ... + (1/N) donde N es el número que el usuario ingrese
+'''Escriba un programa que, al recibir como dato un número entero positivo N, calcule el resultado de la siguiente serie:
+1 + (1/2) + (1/3) + (1/4) + ... + (1/N) donde N es el número que el usuario ingresa
 Si el usuario escribe un número incorrecto, el programa no se ejecuta. En cambio, pregunta de nuevo por la información hasta que el dato ingresado sea correcto.'''
 
 print("BIENVENIDO AL PROGRAMA\n")
 
 while True:
     try:
-        num = int(input("Ingrese un número positivo: "))
-        if num <= 0:
+        n = int(input("Ingresa un número positivo: "))
+        if n <= 0:
             raise ValueError
-            break
-        except ValueError
-        print("Por favor ingrese un número positivo válido: ") 
+        break
+    except ValueError:
+        print("Por favor ingresa un número positivo válido.")
 
 suma = 0
- for i in range (1, num + 1):
-            suma += 1/
+for i in range(1, n+1):
+    suma += 1/i
 
- print("El resultado de la suma de la serie es: ", suma)
+print("La suma de la serie es:", suma)
 
-print("\nFIN DEL PROGRAMA")
+print("\nFIN DEL PROGRAMA") 
+
